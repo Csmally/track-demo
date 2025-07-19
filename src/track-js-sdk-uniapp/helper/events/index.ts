f58380aed69ad0e7c33732d7efa538e7042ca9a4
Csmally <1130180692@qq.com>
2025-07-20 06:56:48 +0800

    添加注释

--- src/track-js-sdk-uniapp/helper/events/index.ts
+++ src/track-js-sdk-uniapp/helper/events/index.ts
@@ -3,11 +3,13 @@ export default class EventHandler {
   private eventHandler: Map<string, Map<string, Function>>;
 
   constructor() {
+    // eventHandler是一个二维Map
     this.eventHandler = new Map();
   }
 
   addEventListener(event: string, func: Function): void {
     const funcs = this.eventHandler.get(event) || new Map();
+    // 不覆盖func.constructor()创建一个新的空匿名函数
     funcs.set(func.constructor(), func);
     this.eventHandler.set(event, funcs);
   }
